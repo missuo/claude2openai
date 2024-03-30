63c0122de5daad093b01aacbe11d283281e30168
Vincent Yang <missuo@pm.me>
2024-03-29 23:19:26 -0400

    init

--- main.go
+++ main.go
@@ -12,49 +12,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type OpenAIRequest struct {
-	Model    string `json:"model"`
-	Messages []struct {
-		Role    string `json:"role"`
-		Content string `json:"content"`
-	} `json:"messages"`
-	Stream bool `json:"stream"`
-}
-
-type ClaudeAPIResponse struct {
-	Content []struct {
-		Text string `json:"text"`
-	} `json:"content"`
-	ID    string `json:"id"`
-	Model string `json:"model"`
-	Usage struct {
-		InputTokens  int `json:"input_tokens"`
-		OutputTokens int `json:"output_tokens"`
-	} `json:"usage"`
-}
-
-type OpenAIResponse struct {
-	ID                string `json:"id"`
-	Object            string `json:"object"`
-	Created           int64  `json:"created"`
-	Model             string `json:"model"`
-	SystemFingerprint string `json:"system_fingerprint,omitempty"`
-	Choices           []struct {
-		Index   int `json:"index"`
-		Message struct {
-			Role    string `json:"role"`
-			Content string `json:"content"`
-		} `json:"message"`
-		Logprobs     interface{} `json:"logprobs"`
-		FinishReason string      `json:"finish_reason"`
-	} `json:"choices"`
-	Usage struct {
-		PromptTokens     int `json:"prompt_tokens"`
-		CompletionTokens int `json:"completion_tokens"`
-		TotalTokens      int `json:"total_tokens"`
-	} `json:"usage"`
-}
-
 func proxyToClaude(c *gin.Context) {
 	var openAIReq OpenAIRequest
 
---
+++ types.go
@@ -0,0 +1,55 @@
+/*
+ * @Author: Vincent Yang
+ * @Date: 2024-03-29 23:11:29
+ * @LastEditors: Vincent Yang
+ * @LastEditTime: 2024-03-29 23:11:44
+ * @FilePath: /claude2openai/types.go
+ * @Telegram: https://t.me/missuo
+ * @GitHub: https://github.com/missuo
+ *
+ * Copyright © 2024 by Vincent, All Rights Reserved.
+ */
+package main
+
+type OpenAIRequest struct {
+	Model    string `json:"model"`
+	Messages []struct {
+		Role    string `json:"role"`
+		Content string `json:"content"`
+	} `json:"messages"`
+	Stream bool `json:"stream"`
+}
+
+type ClaudeAPIResponse struct {
+	Content []struct {
+		Text string `json:"text"`
+	} `json:"content"`
+	ID    string `json:"id"`
+	Model string `json:"model"`
+	Usage struct {
+		InputTokens  int `json:"input_tokens"`
+		OutputTokens int `json:"output_tokens"`
+	} `json:"usage"`
+}
+
+type OpenAIResponse struct {
+	ID                string `json:"id"`
+	Object            string `json:"object"`
+	Created           int64  `json:"created"`
+	Model             string `json:"model"`
+	SystemFingerprint string `json:"system_fingerprint,omitempty"`
+	Choices           []struct {
+		Index   int `json:"index"`
+		Message struct {
+			Role    string `json:"role"`
+			Content string `json:"content"`
+		} `json:"message"`
+		Logprobs     interface{} `json:"logprobs"`
+		FinishReason string      `json:"finish_reason"`
+	} `json:"choices"`
+	Usage struct {
+		PromptTokens     int `json:"prompt_tokens"`
+		CompletionTokens int `json:"completion_tokens"`
+		TotalTokens      int `json:"total_tokens"`
+	} `json:"usage"`
+}
